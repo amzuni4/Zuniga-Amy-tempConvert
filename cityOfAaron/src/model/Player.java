79533d7e1ad21f5ad9b19f3929bd48e6c5dfbc09
Zuniga- Amy <35243042+amzuni4@users.noreply.github.com>
2018-10-09 18:08:11 -0700

    260 Week 02: Program 01:Convert Temperatures

---
+++ cityOfAaron/src/model/Player.java
@@ -0,0 +1,13 @@
+/*
+ * This program starts the game called City of Aaron
+ * Amy Zuniga, Raylee Haws, and Jeremi Lynch Corraborated on the Program 
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class Player {
+    //Add the code for your Player Class here
+}
